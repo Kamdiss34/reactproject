87055552eab104f202d99085960d801d2d47f1ba
kamal <adissakamal0@gmail.com>
2023-11-01 08:48:18 +0100

    added file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importez Bootstrap CSS une seule fois ici
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+reportWebVitals();
